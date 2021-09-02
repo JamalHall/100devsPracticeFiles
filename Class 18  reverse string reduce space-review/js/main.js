d8f92e136e4df5a272740d3cf8ab8ea1ae943051
JHall <JamalHallWebDev@gmail.com>
2021-09-01 21:56:26 -0400

    #100devs We go get

---
+++ Class 18  reverse string reduce space-review/js/main.js
@@ -0,0 +1,48 @@
+//Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arrNum = [1,2,4,5,8,5,10]
+ 
+let total=0 
+for(i of arrNum){
+    total = i+total
+    }
+console.log(total)
+
+let reduceFunction = arrNum.reduce((acc,cv)=>{return acc+cv})
+ console.log(reduceFunction)
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function numSq(arr){
+    let squared = arr.map((e,i) => {
+        let math = e*e
+        return math})
+    console.log(squared)
+}
+numSq([1,2,5,6])
+//Create a function that takes string
+//Print the reverse of that string to the console
+
+function revStr(word){
+    let strArr = word.split('')
+    let reversed = strArr.reverse()
+    let joined = reversed.join('')
+    console.log(joined)
+    return joined
+}
+revStr('hello')
+
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+function bobPal(palStr){
+    let palStr2 = revStr(palStr)
+    if(palStr===palStr2) {
+        alert('palindrome')
+    } else { 
+        alert('Not palindrome')
+    }
+    }
+
+bobPal(prompt('enter word'))
