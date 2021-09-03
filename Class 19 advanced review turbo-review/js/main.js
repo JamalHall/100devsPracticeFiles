e2d2f859982181f9a6dcbeb81b3d0ed4a8bced74
JHall <JamalHallWebDev@gmail.com>
2021-09-02 22:18:34 -0400

    refactored more elegant code spaced repetition

---
+++ Class 19 advanced review turbo-review/js/main.js
@@ -0,0 +1,70 @@
+// *Variables*
+// Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+let isQuestion = 'This is a sentence right?'
+isQuestion.slice(isQuestion.length-1)==='?' ? alert('This is a Question'): alert('This is not a Question')
+
+//Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+let multiWord = 'I am a jr dev.'
+
+function replace(string){
+    let delWord = 'jr dev'
+    if (string.includes(delWord)) {
+        multiWord = multiWord.replace(delWord,'software engineer')
+        console.log(multiWord)
+    }
+}
+replace(multiWord)
+
+// *Functions*
+// Create a function that returns rock, paper, or scissors as randomly as possible
+
+function loop(i){
+    while (i>0) {
+    i--
+    rpsGame()
+    }
+}
+loop(3)
+
+function rpsGame(){
+    let random = Math.round(Math.random()*100)
+    console.log(random)
+    let bot
+       
+        if(random<33){
+            //console.log('rock')
+            bot = 'rock'
+        } else if(random<66) {
+            //console.log('paper')  
+            bot ='paper'
+        } else {
+            //console.log('sissors')  
+            bot = 'sissors'
+        }
+    console.log(bot)
+    return bot
+} 
+  
+// *Conditionals*
+//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+
+let human = prompt('rock paper or sissors').toLowerCase()
+checkWin(human)
+function checkWin(value){
+let bot = rpsGame()
+if(value==='rock' && bot==='sissors' || value==='paper' && bot==='rock' || value==='sissors' && bot==='paper') {
+    alert('you win')
+    } else if (value===bot) {
+        alert('you tied')
+    } else {
+        alert('you lost')
+    }
+}
+ 
+//*Loops*
+//Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+let arrValue = ['paper', 'rock', 'rock', 'sissors']
+function runArray(){
+    arrValue.forEach((e,i) => checkWin(e))
+}
+runArray()
